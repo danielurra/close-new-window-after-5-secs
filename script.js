e6361aa477f4d7ee21281812b60e052b5fb23ede
Daniel Urra <danielurra75@gmail.com>
2023-12-25 18:13:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function opNeWi(){
     // console.log("Onclick event is working");
     let ciscoarUrl = 'https://www.ciscoar.com';
-    let opNeWi = window.open(ciscoarUrl, "", "width=800, height=600");
+    let opNeWi = window.open(ciscoarUrl, "", "width=1100, height=600");
     setTimeout(() => {
         opNeWi.close();
     }, 5000);   
