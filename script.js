31cbf3a3364385694cb1f0c9d2039920b277c684
Daniel Urra <danielurra75@gmail.com>
2023-12-25 18:12:18 -0500

    small change in heighr value

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function opNeWi(){
     // console.log("Onclick event is working");
     let ciscoarUrl = 'https://www.ciscoar.com';
-    let opNeWi = window.open(ciscoarUrl, "", "width=800, height=400");
+    let opNeWi = window.open(ciscoarUrl, "", "width=800, height=600");
     setTimeout(() => {
         opNeWi.close();
     }, 5000);   
